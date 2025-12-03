b4011396a1b1ae40d54e9c5e05a7b365f0aeee6b
AryaXploit <bhattarya190@gmail.com>
2025-12-03 21:11:26 +0545

    Update index.c

--- index.c
+++ index.c
@@ -166,3 +166,52 @@ int main() {
 }
 
 
+ /*  1
+  2  2
+  3  3  3
+  4  4  4  4
+  5  5  5  5  5 */
+
+/*  #include<stdio.h>
+ int main(){
+    for(int i = 1; i<=5; i++){
+        for(int j = 1; j<=i; j++){
+            printf("%3d",i);
+        }
+        printf("\n");
+    }
+ } */
+
+
+
+ /*  1
+  2  2
+  3  3  3
+  4  4  4  4
+  5  5  5  5  5 */
+
+/*   #include<stdio.h>
+ int main(){
+    for(int i = 1; i<=5; i++){
+        for(int j = 1; j<=i; j++){
+            printf("%3d",i);
+        }
+        printf("\n");
+    }
+ } */
+
+
+//    5
+//   5  4
+//   5  4  3
+//   5  4  3  2
+//   5  4  3  2  1
+/*    #include<stdio.h>
+ int main(){
+    for(int i = 5; i>=1; i--){
+        for(int j = 5; j>=i; j--){
+            printf("%3d",j);
+        }
+        printf("\n");
+ }
+} */
